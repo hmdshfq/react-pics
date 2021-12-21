2028f2e15bda5c32c40a7d33cd34a8d58f8bc84a
Hammad <hmdshfq@outlook.com>
2021-12-21 22:49:08 +0100

    simplify function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term)
-    }
     render() {
         return (
             <div className='ui container' style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={ this.onSearchSubmit } />
+                <SearchBar onSubmit={ (term)=> console.log(term) } />
             </div>
         )
     }
