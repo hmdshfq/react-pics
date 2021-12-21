ad00fe5225347a0b048ca59c57741816c16994a0
Hammad <hmdshfq@outlook.com>
2021-12-21 23:55:15 +0100

    add unsplash and axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
+    onSubmitSearch(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID E2lShl2PqlL09IaAMWwduyIEfENQp8MHHGGz0vbI7wQ',
+            }
+        })
+    }
     render() {
         return (
             <div className='ui container' style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={ (term)=> console.log(term) } />
+                <SearchBar onSubmit={this.onSubmitSearch} />
             </div>
         )
     }
