87fcbe6f17696464683ce996a62e0363264504df
Hammad <hmdshfq@outlook.com>
2021-12-21 20:25:40 +0100

    add prevent default

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,14 +5,16 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form onSubmit={e => e.preventDefault()} className='ui form'>
                     <div className='field'>
                         <label htmlFor='search'>Image Search</label>
                         {/* onChange is a special property that listens to user input */}
                         <input
                             id='search'
                             type='text'
-                            onChange={e => this.setState({ term: e.target.value })}
+                            onChange={e =>
+                                this.setState({ term: e.target.value })
+                            }
                             value={this.state.term}
                         />
                     </div>
