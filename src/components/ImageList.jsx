d5d8d6b0d4bcbf3c433c78ddfe225b99a3e9b611
Hammad <hmdshfq@outlook.com>
2021-12-22 17:36:49 +0100

    destructure

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const ImageList = props => {
-    console.log(props.images)
-    const images = props.images.map(image=><img src={image.urls.regular} alt={image.alt_description}></img>)
-    return (
-        <div>{images}</div> 
-    ) 
+    const images = props.images.map(({ id, urls, alt_description }) => {
+        return (
+            <img
+                key={id}
+                src={urls.regular}
+                alt={alt_description}
+            ></img>
+        )
+    })
+    return <div>{images}</div>
 }
 
 export default ImageList
