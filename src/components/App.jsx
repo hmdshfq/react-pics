8938d9575d11c23333aab982e310f6823ce02811
Hammad <hmdshfq@outlook.com>
2021-12-22 12:53:39 +0100

    display images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar'
+import ImageList from './ImageList'
 
 class App extends React.Component {
     state = { images: [] }
     onSubmitSearch = async term => {
-        const response = await unsplash.get(
-            '/search/photos',
-            {
-                params: { query: term },
-            }
-        )
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term },
+        })
         this.setState({ images: response.data.results })
     }
     render() {
         return (
             <div className='ui container' style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSubmitSearch} />
-                Found: {this.state.images.length} images
+                <ImageList images={this.state.images} />
             </div>
         )
     }
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const ImageList = () => {
+const ImageList = props => {
+    console.log(props.images)
+    const images = props.images.map(image=><img src={image.urls.regular} alt={image.alt_description}></img>)
     return (
-        <div>
-            
-        </div>
-    )
+        <div>{images}</div> 
+    ) 
 }
 
 export default ImageList
