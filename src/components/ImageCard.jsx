69afb83ad15df7996ce6f7fd9a84c1c485f7ba39
Hammad <hmdshfq@outlook.com>
2021-12-22 19:26:49 +0100

    add ref

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,15 +3,21 @@ import React from 'react'
 class ImageCard extends React.Component {
     constructor(props) {
         super(props)
+        this.state = { spans: 0 }
         this.imageRef = React.createRef()
     }
     componentDidMount() {
-        console.log(this.imageRef)
+        this.imageRef.current.addEventListener('load', this.setSpans)
+    }
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight
+        const spans = Math.ceil(height / 150)
+        this.setState({ spans })
     }
     render() {
         const { description, urls } = this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd: `spans ${this.state.spans}`}}>
                 <img
                     ref={this.imageRef}
                     src={urls.regular}
