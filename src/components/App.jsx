092b401f5ed09b3310b6520a5fe5e9d5d89df8dc
Hammad <hmdshfq@outlook.com>
2021-12-23 17:42:35 +0100

    create masonry grid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,14 @@ import ImageList from './ImageList'
 
 class App extends React.Component {
     state = { images: [] }
+
     onSubmitSearch = async term => {
         const response = await unsplash.get('/search/photos', {
             params: { query: term },
         })
         this.setState({ images: response.data.results })
     }
+    
     render() {
         return (
             <div className='ui container' style={{ marginTop: '10px' }}>
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -11,13 +11,13 @@ class ImageCard extends React.Component {
     }
     setSpans = () => {
         const height = this.imageRef.current.clientHeight
-        const spans = Math.ceil(height / 150)
+        const spans = Math.ceil(height / 10 + 1)
         this.setState({ spans })
     }
     render() {
         const { description, urls } = this.props.image;
         return (
-            <div style={{ gridRowEnd: `spans ${this.state.spans}`}}>
+            <div style={{ gridRowEnd: `span ${this.state.spans}`}}>
                 <img
                     ref={this.imageRef}
                     src={urls.regular}
